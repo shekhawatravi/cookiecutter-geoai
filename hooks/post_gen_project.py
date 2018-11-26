e1a6c9eabdb63e855fa0eb790dadc7d937664c36
Joel McCune <knu2xs@gmail.com>
2018-11-26 10:54:28 -0800

    arcpy optional in post generation hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,45 +1,64 @@
-import arcpy
-import os.path
+import os
 import shutil
 
-# create locations 
-existing_project_path = os.path.abspath(r'./arcgis/cookiecutter.aprx')
-new_project_path = os.path.abspath(r'./arcgis/{{ cookiecutter.project_name }}.aprx')
-    
-# copy the existing template project
-old_aprx = arcpy.mp.ArcGISProject(existing_project_path)
-old_aprx.saveACopy(new_project_path)
-
-# now create a reference to the new project
-new_aprx = arcpy.mp.ArcGISProject(new_project_path)
+# see if arcpy available to accommodate non-windows environments
+try:
+    import arcpy
+    has_arcpy = True
+except:
+    has_arcpy = False
 
 # set the default geodatabase to be an interim file geodatabase
-dir_lst = [os.path.join(os.getcwd(), 'data', drctry) for drctry in ['raw', 'external', 'interim', 'processed']]
+dir_lst = [os.path.join(os.getcwd(), 'data', drctry)
+           for drctry in ['raw', 'external', 'interim', 'processed']]
 for drctry in dir_lst:
     if not os.path.exists(drctry):
         os.makedirs(drctry)
 
-interim_gdb = os.path.join(dir_lst[2], 'interim.gdb')
-if not arcpy.Exists(interim_gdb):
-    arcpy.management.CreateFileGDB(dir_lst[2], 'interim.gdb')
+# if arcpy available
+if has_arcpy:
 
-new_aprx.defaultGeodatabase = interim_gdb
+    # create locations
+    existing_project_path = os.path.abspath(r'./arcgis/cookiecutter.aprx')
+    new_project_path = os.path.abspath(r'./arcgis/{{ cookiecutter.project_name }}.aprx')
 
-# create a matched name toolbox, and set the new project to reference it
-new_name = os.path.basename(new_project_path).split('.')[0]
+    # copy the existing template project
+    old_aprx = arcpy.mp.ArcGISProject(existing_project_path)
+    old_aprx.saveACopy(new_project_path)
 
-new_toolbox_path = os.path.abspath(os.path.join(
-    os.path.dirname(new_project_path),
-    new_name + '.tbx'
-))
+    # now create a reference to the new project
+    new_aprx = arcpy.mp.ArcGISProject(new_project_path)
 
-shutil.copyfile(old_aprx.defaultToolbox, new_toolbox_path)
+    # create the file geodatabase
+    interim_gdb = os.path.join(dir_lst[2], 'interim.gdb')
+    if not arcpy.Exists(interim_gdb):
+        arcpy.management.CreateFileGDB(dir_lst[2], 'interim.gdb')
 
-new_aprx.defaultToolbox = new_toolbox_path
+    # set the default file geodatabase for the aprx
+    new_aprx.defaultGeodatabase = interim_gdb
 
-# save new settings for aprx
-new_aprx.save()
+    # create a matched name toolbox, and set the new project to reference it
+    new_name = os.path.basename(new_project_path).split('.')[0]
 
-# now delete the old toolbox and project
-arcpy.management.Delete(old_aprx.defaultToolbox)
-arcpy.management.Delete(existing_project_path)
+    # create the new toolbox path
+    new_toolbox_path = os.path.abspath(os.path.join(
+        os.path.dirname(new_project_path),
+        new_name + '.tbx'
+    ))
+
+    # copy the toolbox to the new location
+    shutil.copyfile(old_aprx.defaultToolbox, new_toolbox_path)
+
+    # update the pro project toolbox path
+    new_aprx.defaultToolbox = new_toolbox_path
+
+    # save new settings for aprx
+    new_aprx.save()
+
+    # now delete the old toolbox and project
+    os.remove(old_aprx.defaultToolbox)
+    os.remove(existing_project_path)
+
+# if arcpy is not available
+else:
+    os.remove(os.path.abspath(r'./arcgis'))
